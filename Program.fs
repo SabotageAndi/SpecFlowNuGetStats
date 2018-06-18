664c570ba3cdecca3a971d29686da79aed33b760
Andreas Willich <andreas.willich@techtalk.at>
2018-06-18 17:31:59 +0200

    add files!

---
+++ Program.fs
@@ -0,0 +1,28 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open FSharp.Data
+
+
+type NugetStats = HtmlProvider<"https://www.nuget.org/packages/SpecFlow">
+
+type VersionDownloads =
+    {
+        Version: string
+        Downloads: decimal
+    }
+
+
+let parseVersion (version : string) = 
+    version.Replace("(current)", "").Trim()
+
+[<EntryPoint>]
+let main argv =
+
+    let rawStats = NugetStats().Tables.``Version History``
+
+    rawStats.Rows 
+        |> Seq.map (fun r -> {VersionDownloads.Version = parseVersion r.Version2; Downloads = r.Downloads})
+        |> Seq.iter (fun r -> printfn "Version: %s; Downloads: %f" r.Version r.Downloads)
+
+    0 // return an integer exit code
